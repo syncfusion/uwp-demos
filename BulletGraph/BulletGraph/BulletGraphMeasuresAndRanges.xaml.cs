97a53024eda48249637f7c69df8241f86d99eecc
Build Automation <buildautomation@syncfusion.com>
2019-08-23 11:07:46 +0530

    v17.2.0.46

---
+++ BulletGraph/BulletGraph/BulletGraphMeasuresAndRanges.xaml.cs
@@ -0,0 +1,75 @@
+#region Copyright Syncfusion Inc. 2001-2019.
+// Copyright Syncfusion Inc. 2001-2019. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BulletGraphUWP_Samples
+{
+    public sealed partial class BulletGraphMeasuresAndRanges : SampleLayout,IDisposable
+    {
+        Dictionary<string, Windows.UI.Color> colorsList = new Dictionary<string, Windows.UI.Color>();
+
+        public BulletGraphMeasuresAndRanges()
+        {
+            this.InitializeComponent();
+
+            Type colorType = typeof(Windows.UI.Colors);
+            IEnumerable<PropertyInfo> propInfos = colorType.GetRuntimeProperties();
+            foreach (PropertyInfo propInfo in propInfos)
+            {
+                colorsList.Add(propInfo.Name, (Windows.UI.Color)propInfo.GetValue(colorType));
+            }
+
+            cmb_Range1Stroke.ItemsSource = cmb_Range2Stroke.ItemsSource = cmb_Range3Stroke.ItemsSource = colorsList.Keys;
+
+            if (!Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                this.SfBulletGraph.QuantitativeScaleLength = 450;
+            }
+        }
+
+
+        private void cmb_Range1Stroke_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            SfBulletGraph.QualitativeRanges[0].RangeStroke = new SolidColorBrush(colorsList[cmb_Range1Stroke.SelectedItem.ToString()]);
+        }
+
+        private void cmb_Range2Stroke_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            SfBulletGraph.QualitativeRanges[1].RangeStroke = new SolidColorBrush(colorsList[cmb_Range2Stroke.SelectedItem.ToString()]);
+        }
+
+        private void cmb_Range3Stroke_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            SfBulletGraph.QualitativeRanges[2].RangeStroke = new SolidColorBrush(colorsList[cmb_Range3Stroke.SelectedItem.ToString()]);
+        }
+
+        public override void Dispose()
+        {
+            this.SfBulletGraph.Dispose();
+            this.SfBulletGraph = null;
+        }
+       
+    }
+}
---
+++ Navigation/BusyIndicator/BusyIndicatorView.xaml.cs
@@ -0,0 +1,72 @@
+#region Copyright Syncfusion Inc. 2001-2019.
+// Copyright Syncfusion Inc. 2001-2019. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Common;
+using Syncfusion.UI.Xaml.Controls.Notification;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Notification.BusyIndicator
+{
+    public sealed partial class BusyIndicatorView : SampleLayout
+    {
+        public BusyIndicatorView()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public static bool IsMobileFamily()
+        {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                return true;
+            }
+            return false;
+        }
+        private AnimationTypes animation;
+
+        public AnimationTypes Animation
+        {
+            get { return animation; }
+            set { animation = value; }
+        }
+
+       
+        public override void Dispose()
+        {
+            if (busy != null)
+            {
+                busy.Dispose();
+                busy = null;
+            }
+            
+            this.DataContext = null;
+        }
+
+
+        private void discard_Click(object sender, RoutedEventArgs e)
+        {
+            this.controlView.Visibility = Visibility.Visible;
+          
+        }
+    }
+}
