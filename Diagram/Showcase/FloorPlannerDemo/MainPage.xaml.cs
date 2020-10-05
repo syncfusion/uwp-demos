f5772d397180916ccee171b0ea5b0a00bde8b8e5
SyncfusionInstall <buildautomation@syncfusion.com>
2020-10-05 15:23:23 +0530

    v18.3.0.35

---
+++ Diagram/Showcase/FloorPlannerDemo/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+#region Copyright Syncfusion Inc. 2001-2020.
+// Copyright Syncfusion Inc. 2001-2020. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WorkFlowEditor;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FloorPlannerDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FloorPlannerViewModel floor = new FloorPlannerViewModel();
+            floor.GoBack = new DelegateCommand<object>(OnGoBack);
+            this.DataContext =floor;
+        }
+        private void OnGoBack(object parameter)
+        {
+            FloorPlannerViewModel floor = this.DataContext as FloorPlannerViewModel;
+            floor.GoBack = null;
+            this.DataContext = null;
+            //Frame.Navigate(typeof(SampleBrowser.Home));
+        }
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
