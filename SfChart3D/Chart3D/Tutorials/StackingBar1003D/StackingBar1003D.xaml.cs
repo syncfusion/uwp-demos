aea2afc4c990fe435a9327c49e8baa33b42b687d
buildautomation <buildautomation@syncfusion.com>
2021-08-17 17:03:07 +0530

    v19.2.0.55

---
+++ SfChart3D/Chart3D/Tutorials/StackingBar1003D/StackingBar1003D.xaml.cs
@@ -0,0 +1,87 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Common;
+using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Profile;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Syncfusion.SampleBrowser.UWP.SfChart3D
+{
+    public sealed partial class StackingBar1003D : SampleLayout
+    {
+        public StackingBar1003D()
+        {
+            this.InitializeComponent();
+
+            StackingBar100Chart3D.Rotation = 24;
+            sb100ChartAdornmentInfo3D1.SegmentLabelContent = LabelContent.LabelContentPath;
+            sb100ChartAdornmentInfo3D1.AdornmentsPosition = AdornmentsPosition.TopAndBottom;
+            sb100ChartAdornmentInfo3D1.LabelTemplate = grid.Resources["labelTemplate1"] as DataTemplate;
+            sb100ChartAdornmentInfo3D1.HorizontalAlignment = HorizontalAlignment.Center;
+            sb100ChartAdornmentInfo3D1.VerticalAlignment = VerticalAlignment.Center;
+            sb100ChartAdornmentInfo3D1.ShowLabel = true;
+
+            sb100ChartAdornmentInfo3D2.SegmentLabelContent = LabelContent.LabelContentPath;
+            sb100ChartAdornmentInfo3D2.AdornmentsPosition = AdornmentsPosition.TopAndBottom;
+            sb100ChartAdornmentInfo3D2.LabelTemplate = grid.Resources["labelTemplate1"] as DataTemplate;
+            sb100ChartAdornmentInfo3D2.HorizontalAlignment = HorizontalAlignment.Center;
+            sb100ChartAdornmentInfo3D2.VerticalAlignment = VerticalAlignment.Center;
+            sb100ChartAdornmentInfo3D2.ShowLabel = true;
+
+            sb100ChartAdornmentInfo3D3.SegmentLabelContent = LabelContent.LabelContentPath;
+            sb100ChartAdornmentInfo3D3.AdornmentsPosition = AdornmentsPosition.TopAndBottom;
+            sb100ChartAdornmentInfo3D3.LabelTemplate = grid.Resources["labelTemplate1"] as DataTemplate;
+            sb100ChartAdornmentInfo3D3.HorizontalAlignment = HorizontalAlignment.Center;
+            sb100ChartAdornmentInfo3D3.VerticalAlignment = VerticalAlignment.Center;
+            sb100ChartAdornmentInfo3D3.ShowLabel = true;
+
+            if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
+            {
+                StackingBar100Chart3D.Margin = new Thickness(10);
+            }
+            else
+            {
+                StackingBar100Chart3D.Margin = new Thickness(70, 20, 75, 25);
+            }
+        }
+
+        public override void Dispose()
+        {
+            if (this.grid.DataContext is CategoryDataViewModel)
+                (this.grid.DataContext as CategoryDataViewModel).Dispose();
+
+            if (this.StackingBar100Chart3D != null)
+            {
+                foreach (var series in this.StackingBar100Chart3D.Series)
+                {
+                    series.ClearValue(ChartSeriesBase.ItemsSourceProperty);
+                }
+                this.StackingBar100Chart3D = null;
+            }
+
+            this.grid.Resources = null;
+
+            base.Dispose();
+        }
+    }
+}
---
+++ SfDataGrid/Tutorials/HelperClass/StyleSelectors.cs
@@ -0,0 +1,108 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Data;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Syncfusion.SampleBrowser.UWP.SfDataGrid;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// This class represents the StyleSelector for TableSummary
+    /// </summary>
+    class TableSummaryStyleSelectorsforConditionalFormating : StyleSelector
+    {
+        /// <summary>
+        /// Converts the SummaryRow into Style
+        /// </summary>
+        protected override Windows.UI.Xaml.Style SelectStyleCore(object item, Windows.UI.Xaml.DependencyObject container)
+        {
+            if ((item as SummaryRecordEntry).SummaryRow.ShowSummaryInRow)
+            {
+                var res = new DataGrid.ConditionalFormatting();
+                var col = res.Resources["tableSummaryCell"] as Style;
+                if (col != null)
+                    return col;
+               
+            }
+            else
+            {
+                var res = new DataGrid.ConditionalFormatting();
+                var col = res.Resources["normaltableSummaryCell"] as Style;
+                if (col != null)
+                    return col;
+              
+            }
+            return base.SelectStyleCore(item, container);
+        }
+    }
+#if !SFDATAGRID_STORE
+    /// <summary>
+    /// This class represents the StyleSelector for TableSummary
+    /// </summary>
+    class TableSummaryStyleSelectorsforSummaries : StyleSelector
+    {
+        /// <summary>
+        /// Converts the SummaryRow into Style
+        /// </summary>
+        protected override Windows.UI.Xaml.Style SelectStyleCore(object item, Windows.UI.Xaml.DependencyObject container)
+        {
+            if ((item as SummaryRecordEntry).SummaryRow.ShowSummaryInRow)
+            {
+                var res = new DataGrid.Summaries();
+                var style = res.Resources["tableSummaryCell"] as Style;
+                if (style != null)
+                    return style;
+
+            }
+            else
+            {
+                var res = new DataGrid.Summaries();
+                var style = res.Resources["normaltableSummaryCell"] as Style;
+                if (style != null)
+                    return style;
+
+            }
+            return base.SelectStyleCore(item, container);
+        }
+    }
+    /// <summary>
+    /// This class represents the StyleSelector for GroupSummary
+    /// </summary>
+    class GroupSummaryStleSelector : StyleSelector
+    {
+        /// <summary>
+        /// Converts the SummaryRow into Style
+        /// </summary>
+        protected override Windows.UI.Xaml.Style SelectStyleCore(object item, Windows.UI.Xaml.DependencyObject container)
+        {
+            if ((item as SummaryRecordEntry).SummaryRow.ShowSummaryInRow)
+            {
+                var res = new DataGrid.Summaries();
+                var style = res.Resources["groupSummaryCell"] as Style;
+                if (style != null)
+                    return style;
+            }
+            else
+            {
+                var res = new DataGrid.Summaries();
+                var style = res.Resources["normalgroupSummaryCell"] as Style;
+                if (style != null)
+                    return style;
+            }
+            return base.SelectStyleCore(item, container);
+        }
+    }        
+#endif
+
+}
