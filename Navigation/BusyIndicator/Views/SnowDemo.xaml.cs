e1f2f9a343f1058ed61492bcbf70306b51ce344b
buildautomation <buildautomation@syncfusion.com>
2021-12-29 13:54:23 +0530

    v19.4.0.38

---
+++ Navigation/BusyIndicator/Views/SnowDemo.xaml.cs
@@ -0,0 +1,36 @@
+#region Copyright Syncfusion Inc. 2001-2021.
+// Copyright Syncfusion Inc. 2001-2021. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Syncfusion.UI.Xaml.Controls.Notification;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Notification.BusyIndicator
+{
+    public sealed partial class SnowDemo: UserControl
+    {
+        public SnowDemo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
