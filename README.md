79690128a680514b7d531ceb19857b77def1ee29
buildautomation <buildautomation@syncfusion.com>
2022-02-01 20:11:13 +0530

    v19.4.0.48

--- README.md
+++ README.md
@@ -413,4 +413,4 @@ The easiest way to use these samples without using Git is to download the zip fi
 
 * To renew the subscription, click [here](https://www.syncfusion.com/sales/products?utm_source=github&utm_medium=listing) or contact our sales team at <salessupport@syncfusion.com>.
   
-<p>Copyright © 2001-2021 Syncfusion, Inc. Updated on 2021-12-29 at precisely 13:54:15 EST.</p> 
+<p>Copyright © 2001-2022 Syncfusion, Inc. Updated on 2022-02-01 at precisely 20:09:00 EST.</p> 
---
+++ SfDataGrid/Tutorials/RowStyle.xaml.cs
@@ -0,0 +1,50 @@
+#region Copyright Syncfusion Inc. 2001-2022.
+// Copyright Syncfusion Inc. 2001-2022. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Common;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DataGrid
+{
+    public sealed partial class RowStyle : SampleLayout
+    {
+        public RowStyle()
+        {
+            this.InitializeComponent();
+            var datacontext = new EmployeeInfoViewModel();
+            this.DataContext = datacontext;
+            this.sfDataGrid.ItemsSource = datacontext.EmployeeDetails200;
+        }        
+        /// <summary>
+        /// Dispose of unmanaged resources. 
+        /// </summary>
+        public sealed override void Dispose()
+        {
+            this.Resources.Clear();         
+            this.sfDataGrid.Dispose();
+            this.sfDataGrid.ItemsSource = null;
+            (this.DataContext as IDisposable).Dispose();
+            this.DataContext = null;
+            base.Dispose();
+        }
+    }
+}
