2c39e9d2bf46ccebddb662722da2b9384b044567
SyncfusionBuild <buildautomation@syncfusion.com>
2023-03-24 09:01:49 +0000

    v21.1.35

---
+++ Layout/Accordion/Accordion.xaml.cs
@@ -0,0 +1,70 @@
+#region Copyright Syncfusion Inc. 2001-2023.
+// Copyright Syncfusion Inc. 2001-2023. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Common;
+using Syncfusion.UI.Xaml.Controls.Layout;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Layout.Accordion
+{
+    public sealed partial class Accordion : SampleLayout
+    {
+        public Accordion()
+        {
+            this.InitializeComponent();
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                Mode.FontSize = 16;
+            }
+        }
+        private void LoadValues(object sender, RoutedEventArgs e)
+        {
+            Mode.Items.Clear();
+            Mode.Items.Add(AccordionSelectionMode.One);
+            Mode.Items.Add(AccordionSelectionMode.OneOrMore);
+            Mode.Items.Add(AccordionSelectionMode.ZeroOrMore);
+            Mode.Items.Add(AccordionSelectionMode.ZeroOrOne);
+            Mode.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //if (this.controlView.Visibility == Visibility.Visible)
+            //{
+            //    this.controlView.Visibility = Visibility.Collapsed;
+            //    this.setting.Visibility = Visibility.Visible;
+            //    (sender as Button).Content = "Done";
+            //}
+            //else
+            //{
+            //    this.controlView.Visibility = Visibility.Visible;
+            //    this.setting.Visibility = Visibility.Collapsed;
+            //    (sender as Button).Content = "Options";
+            //}
+        }
+
+        public override void Dispose()
+        {
+            Mode.Loaded -= LoadValues;
+        }
+    }
+}
---
+++ PdfViewer/PdfViewer/Getting Started/CommentsList/CommentListView.xaml.cs
@@ -0,0 +1,82 @@
+#region Copyright Syncfusion Inc. 2001-2023.
+// Copyright Syncfusion Inc. 2001-2023. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PdfViewerDemo
+{
+    public sealed partial class CommentListView : UserControl
+    {
+        public CommentListView()
+        {
+            this.InitializeComponent();
+            if(MainPageAccesser.MainPage.SelectedCommentListView != null)
+            {
+                MainPageAccesser.MainPage.SelectedCommentListView.HideList();
+            }
+            MainPageAccesser.MainPage.SelectedCommentListView = this;
+        }
+
+        void ShowList()
+        {
+            if (MainPageAccesser.MainPage.SelectedCommentListView != null && MainPageAccesser.MainPage.SelectedCommentListView != this)
+                MainPageAccesser.MainPage.SelectedCommentListView.HideList();
+            listView.Visibility = Visibility.Visible;
+            expandIcon.Visibility = Visibility.Collapsed;
+            collapseIcon.Visibility = Visibility.Visible;
+            MainPageAccesser.MainPage.SelectedCommentListView = this;
+        }
+
+        void HideList()
+        {
+            listView.Visibility = Visibility.Collapsed;
+            expandIcon.Visibility = Visibility.Visible;
+            collapseIcon.Visibility = Visibility.Collapsed;
+            MainPageAccesser.MainPage.SelectedCommentListView = null;
+        }
+
+        internal ListView CommentList
+        {
+            get
+            {
+                return listView;
+            }
+        }
+
+        void ToggleListView()
+        {
+            if (listView.Visibility == Visibility.Collapsed)
+                ShowList();
+            else
+                HideList();
+        }
+        private void expandButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleListView();
+        }
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ToggleListView();
+        }
+    }
+}
