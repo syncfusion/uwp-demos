b07276a5f4b4ee0985e901f73b4c205ac14a4129
buildautomation <buildautomation@syncfusion.com>
2024-06-13 09:19:30 +0000

    v26.1.35

---
+++ Navigation/GroupBar/ViewModel/NoteViewModel.cs
@@ -0,0 +1,41 @@
+#region Copyright Syncfusion Inc. 2001-2024.
+// Copyright Syncfusion Inc. 2001-2024. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBrowser.GroupBar
+{
+    public class NoteViewModel
+    {
+        private ObservableCollection<Note> notes;
+
+        public ObservableCollection<Note> Notes
+        {
+            get { return notes; }
+            set { notes = value; }
+        }
+
+        public NoteViewModel()
+        {
+            notes = GetNotes();
+        }
+        private ObservableCollection<Note> GetNotes()
+        {
+            ObservableCollection<Note> _notes = new ObservableCollection<Note>();
+            _notes.Add(new Note() { Message = "Meeting with robb at 7PM" });
+            _notes.Add(new Note() { Message = "Package delivered on monday" });
+            _notes.Add(new Note() { Message = "Due at tuesday" });
+            _notes.Add(new Note() { Message = "License need to be renewed" });
+            return _notes;
+        }
+    }
+}
---
+++ SfDataGrid/Tutorials/ViewModel/DataBoundViewModel.cs
@@ -0,0 +1,98 @@
+#region Copyright Syncfusion Inc. 2001-2024.
+// Copyright Syncfusion Inc. 2001-2024. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Syncfusion.UI.Xaml.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// This class represents the DataBoundViewModel
+    /// </summary>
+    public class DataBoundViewModel : INotifyPropertyChanged, IDisposable
+    {
+
+        public DataBoundViewModel()
+        {
+
+        }
+
+        private List<OrderInfo> _ordersListDetails = null;
+
+        /// <summary>
+        /// Gets or sets the OrderListDetails.
+        /// </summary>
+        /// <value>The OrderListDetails.</value>
+        public List<OrderInfo> OrdersListDetails
+        {
+            get
+            {
+                if (_ordersListDetails == null)
+                    return new OrderInfoViewModel().OrdersListDetails;
+                else
+                    return _ordersListDetails;
+            }
+        }
+
+        private ObservableCollection<Employees> employeeDetails = null;
+
+        /// <summary>
+        /// Gets or sets the EmployeeDetails.
+        /// </summary>
+        /// <value>The EmployeeDetails.</value>
+        public ObservableCollection<Employees> EmployeeDetails
+        {
+            get
+            {
+                if (employeeDetails == null)
+                    return new EmployeeInfoViewModel().EmployeeDetails;
+                else
+                    return employeeDetails;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool isdisposable)
+        {
+            if (OrdersListDetails != null)
+            {
+                OrdersListDetails.Clear();
+            }
+            if (EmployeeDetails != null)
+            {
+                EmployeeDetails.Clear();
+            }
+        }
+    }
+}
