0f7d23a994cab61fa6205047a45a69ec87b38495
buildautomation <buildautomation@syncfusion.com>
2024-09-23 04:17:02 +0000

    v27.1.48

---
+++ Editor/DateTimePickers/Controls/DateTimeComboProperties.cs
@@ -0,0 +1,70 @@
+#region Copyright Syncfusion Inc. 2001-2024.
+// Copyright Syncfusion Inc. 2001-2024. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Common;
+using Syncfusion.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input.DateTimePickers
+{
+    public class DateTimeComboProperties : NotificationObject
+    {
+        private String dateformatString = "M/d/yyyy";
+
+        public String DateFormat
+        {
+            get { return dateformatString; }
+            set { dateformatString = value; RaisePropertyChanged("DateFormat"); }
+        }   
+
+        private int _minute=5;
+
+        public int Minute
+        {
+            get { return _minute; }
+            set { _minute = value; RaisePropertyChanged("Minute"); }
+        }
+
+        private int _second = 15;
+
+        public int Second
+        {
+            get { return _second; }
+            set { _second = value; RaisePropertyChanged("Second"); }
+        }
+
+        private DateTime _mindate=Convert.ToDateTime("11/9/2012");
+        public DateTime Mindate
+        {
+            get { return _mindate; }
+            set { _mindate = value; RaisePropertyChanged("Mindate"); }
+        }
+
+        private DateTime _maxdate = Convert.ToDateTime("11/9/2014");
+                
+        public DateTime Maxdate
+        {
+            get { return _maxdate; }
+            set { _maxdate = value; RaisePropertyChanged("Maxdate"); }
+        }
+        
+        
+
+        private String timeformatString = "h/m/s/t";
+
+        public String TimeFormat
+        {
+            get { return timeformatString; }
+            set { timeformatString = value; RaisePropertyChanged("TimeFormat"); }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -413,4 +413,4 @@ The easiest way to use these samples without using Git is to download the zip fi
 
 * To renew the subscription, click [here](https://www.syncfusion.com/sales/products?utm_source=github&utm_medium=listing) or contact our sales team at <salessupport@syncfusion.com>.
   
-<p>Copyright © 2001-2024 Syncfusion, Inc. Updated on 2024-07-25 at precisely 14:56:00 EST.</p>
+<p>Copyright © 2001-2024 Syncfusion, Inc. Updated on 2024-09-23 at precisely 04:17:00 EST.</p>
