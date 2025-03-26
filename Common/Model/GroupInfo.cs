7e762c052f1c27d231451874266c73f0c45cb233
buildautomation <buildautomation@syncfusion.com>
2025-03-26 13:21:03 +0000

    v29.1.33

---
+++ Common/Model/GroupInfo.cs
@@ -0,0 +1,25 @@
+#region Copyright Syncfusion® Inc. 2001-2025.
+// Copyright Syncfusion® Inc. 2001-2025. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class GroupInfoList<T> : List<object>
+    {
+        public object Key { get; set; }
+
+        public new IEnumerator<object> GetEnumerator()
+        {
+            return (System.Collections.Generic.IEnumerator<object>)base.GetEnumerator();
+        }
+    }
+}
