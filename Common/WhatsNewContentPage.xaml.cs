b153a16550f0605850548480cfaf7d1327ba2620
buildautomation <buildautomation@syncfusion.com>
2025-06-26 13:40:58 +0000

    v30.1.37

---
+++ Common/WhatsNewContentPage.xaml.cs
@@ -0,0 +1,70 @@
+#region Copyright Syncfusion® Inc. 2001-2025.
+// Copyright Syncfusion® Inc. 2001-2025. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Common
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WhatsNewContentPage : Page
+    {
+        public WhatsNewContentPage()
+        {
+            this.InitializeComponent();
+            if(DeviceFamily.GetDeviceFamily()==Devices.Mobile)
+            {
+                viewContent.Margin = new Thickness(0, 0, 5, -22);
+            }
+           // SystemNavigationManager.GetForCurrentView().BackRequested += WhatsNewContentPage_BackRequested;
+        }
+
+        private void WhatsNewContentPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            FrameNavigationService.GoBack();
+            e.Handled = true;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+           // if (FrameNavigationService.CurrentFrame.CanGoBack)
+           // {
+           //     SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+           //AppViewBackButtonVisibility.Visible;
+           // }
+           // else
+           // {
+           //     SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+           //AppViewBackButtonVisibility.Collapsed;
+           // }
+            var info = e.Parameter as SampleInfo;
+            this.DataContext = info;
+            FrameNavigationService.Header.Text = info.Header;
+            FrameNavigationService.Main.ListSelection = -1;
+            Type type = Type.GetType(info.SampleView.ToString());
+            this.viewContent.Content = Activator.CreateInstance(type) as FrameworkElement;
+             
+        }
+    }
+}
---
+++ SfSpreadsheet/Tutorials/GraphicCell.xaml.cs
@@ -0,0 +1,68 @@
+#region Copyright Syncfusion® Inc. 2001-2025.
+// Copyright Syncfusion® Inc. 2001-2025. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+
+using System;
+using Common;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Reflection;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SpreadsheetSamples
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GraphicCell : SampleLayout, IDisposable
+    {
+        public GraphicCell()
+        {
+            this.InitializeComponent();
+            OpenWorkbook();
+        }
+
+        async void OpenWorkbook()
+        {
+            var assembly = typeof(GraphicCell).GetTypeInfo().Assembly;
+            string resourcePath = "Syncfusion.SampleBrowser.UWP.SfSpreadsheet.Assets.GraphicCell.xlsx";
+            try
+            {
+                using (var fileStream = assembly.GetManifestResourceStream(resourcePath))
+                {
+                    await this.spreadsheet.Open(fileStream);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public override void Dispose()
+        {
+            this.spreadsheet.Dispose();
+            if (this.ribbon != null)
+                this.ribbon.Dispose();
+            Resources.Clear();
+            base.Dispose();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
